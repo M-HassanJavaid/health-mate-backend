6c7b51b5a22613e104fbc8de16d2e9c89cbdd4eb
Your Name <javaidhassan464@gmail.com>
2026-03-04 00:34:10 -0800

    add cors

--- src/server.js
+++ src/server.js
@@ -23,6 +23,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({  extended: true }));
 app.use(cookieParser());
